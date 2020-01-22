0c610dc5f3fc44f672ec142e0c241b4788f3c028
Chase <Strikeeaglechase@gmail.com>
2020-01-22 13:13:56 -0500

    Added social login

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function loginUser(page, user, pass) {
 	});
 }
 
-async function run() {
+async function runGameLoginTest() {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto('https://krunker.io/');
@@ -40,4 +40,39 @@ async function run() {
 	await browser.close();
 };
 
-run();
+async function loginSocial(page, user, pass) {
+	await page.waitForSelector('#profileLogin');
+	var loginBtn = await page.$('#profileLogin');
+	await loginBtn.click();
+
+	await page.waitForSelector('#accName');
+	const userBox = await page.$('#accName');
+	const passBox = await page.$('#accPass');
+	await userBox.click();
+	await page.keyboard.type(user);
+	await passBox.click();
+	await page.keyboard.type(pass);
+
+	var btn = await page.evaluate(() => {
+		var elms = document.getElementsByClassName('accountButton');
+		elms[0].click();
+	});
+
+	await page.waitForSelector('#popupHolder', {
+		visible: true
+	});
+	await page.screenshot({
+		path: 'example.png'
+	});
+}
+
+async function run(user) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('https://krunker.io/social.html?p=profile&q=' + user);
+	await loginSocial(page, USERNAME, PASSWORD);
+	await browser.close();
+};
+
+
+run('Strikeeaglekid');
