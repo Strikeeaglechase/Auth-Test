04c386aba6eff460593c4ec63c822a7813e8a7dd
Chase <Strikeeaglechase@gmail.com>
2020-01-22 12:41:39 -0500

    Finished login

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const USERNAME = 'Strikeeagle2';
 const PASSWORD = '1329043';
 
 async function loginUser(page, user, pass) {
-	await page.screenshot({
-		path: 'example.png'
-	});
 	await page.waitForSelector('#signedOutHeaderBar');
 	var btn = await page.evaluate(() => {
 		var elms = document.getElementsByClassName('button lgn');
 		elms[0].click();
 	});
-	await page.waitForSelector('#menuWindow');
+
+	await page.waitForSelector('#accName');
+	const userBox = await page.$('#accName');
+	const passBox = await page.$('$accPass');
+	await userBox.click();
+	await page.keyboard.type(user);
+	await passBox.click();
+	await page.keyboard.type(pass);
+	var btn = await page.evaluate(() => {
+		var elms = document.getElementsByClassName('accountButton');
+		elms[1].click();
+	});
+	await page.waitForFunction(() => document.getElementById('menuAccountUsername').innerText == user);
 	await page.screenshot({
 		path: 'example.png'
 	});
