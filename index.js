ff1c8a3c38956680535d68f218254a23d23887f9
Chase <Strikeeaglechase@gmail.com>
2020-01-22 12:27:50 -0500

    First PUP test

---
+++ index.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+const USERNAME = 'Strikeeagle2';
+const PASSWORD = '1329043';
+
+async function loginUser(page, user, pass) {
+	await page.waitForSelector('.button lgn');
+	var btn = await page.evaluate(() => {
+		var elms = document.getElementsByClassName('button lgn');
+		elms[0].click();
+	});
+	await page.waitForSelector('#menuWindow');
+	await page.screenshot({
+		path: 'example.png'
+	});
+}
+
+async function run() {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('https://krunker.io/');
+	await loginUser(page, USERNAME, PASSWORD);
+	await browser.close();
+};
